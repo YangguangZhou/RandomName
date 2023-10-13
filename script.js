4c8d7c73d69dcca24d69d9011c7674d2954256c0
Jerry Zhou <61733195+YangguangZhou@users.noreply.github.com>
2023-10-13 13:37:56 +0800

    up

---
+++ script.js
@@ -0,0 +1,29 @@
+// 读取名单文件
+function readNamesFromFile() {
+    fetch('./name.txt')
+      .then(response => response.text())
+      .then(data => {
+        const names = data.split('\n');
+        startButton.addEventListener('click', () => startRolling(names));
+      })
+      .catch(error => console.error(error));
+  }
+  
+  // 开始滚动动画
+  function startRolling(names) {
+    const nameDisplay = document.getElementById('nameDisplay');
+    const rollInterval = setInterval(() => {
+      const randomIndex = Math.floor(Math.random() * names.length);
+      nameDisplay.textContent = names[randomIndex];
+    }, 50);
+  
+    setTimeout(() => {
+      clearInterval(rollInterval);
+    }, 2000);
+  }
+  
+  // 页面加载完成后执行
+  window.onload = function() {
+    const startButton = document.getElementById('startButton');
+    readNamesFromFile();
+  };
